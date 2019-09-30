646a3975be3b081cff14841141787ae0ab0cbee2
nunu2021 <labdhijain753@gmail.com>
2019-09-30 14:25:57 -0500

    lol

---
+++ app/src/main/java/com/example/weightlifting/AddWeight.java
@@ -0,0 +1,14 @@
+package com.example.weightlifting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddWeight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_weight);
+    }
+}
